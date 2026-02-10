45ed7bc5bd191ab27df6e26dbad8fe679a528e04
yuanqh23 <soverreigndrgon@gmail.com>
2026-02-10 10:57:02 +0800

    finish lab 4

---
+++ lab4/flik/FileTest.java
@@ -0,0 +1,14 @@
+package flik;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FileTest {
+
+    @Test
+    public void testIsSameNumber() {
+        assertTrue(Flik.isSameNumber(1, 1));
+    }
+}
